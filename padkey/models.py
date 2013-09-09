7e21f1e5a1d6c3ff243f9c9536a21e56ba513a42
chrisvans <chrisvanschyndel@gmail.com>
2013-09-08 23:13:50 -0400

    Initial Model Created for PadKey

--- padkey/models.py
+++ padkey/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Passcode(models.Model):
+    passcode = models.CharField(max_length=4, null=False)
+    timestamp = models.DateTimeField(auto_now_add=True, null=False)
+
+    def __unicode__():
+        return unicode(passcode) + " " + unicode(timestamp)
